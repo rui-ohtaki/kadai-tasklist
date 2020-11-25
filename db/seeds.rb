7ae76ffea2a97a97c7a2743da7b3c25e9b928a2b
rui.ootaki <rui.ohtaki2@gmail.com>
2020-11-25 11:43:36 +0000

    seed  modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Task.create(status: 'test status 1', content: 'test content 1')
-Task.create(title: 'test title 2', content: 'test content 2')
-Task.create(title: 'test title 3', content: 'test content 3')
+(1..100).each do |number|
+  Task.create(status: 'status' + number.to_s, content: 'test content ' + number.to_s)
+end
